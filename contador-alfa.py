c6efdf1255a2ee03129065b40869b14ad61bd052
leo <164808753+secboyleo@users.noreply.github.com>
2024-06-18 13:35:58 -0300

    Add files via upload

---
+++ contador-alfa.py
@@ -0,0 +1,16 @@
+def conta_letras(frase, contar="vogais"):
+    vogais = 'aeiouAEIOU'
+    consoante = 'bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ'
+    cont_vogal = 0
+    cont_consoante = 0
+    
+    for letra in frase:
+        if letra in vogais:
+            cont_vogal += 1
+        elif letra in consoante:
+            cont_consoante += 1
+            
+    if contar == 'consoantes':
+        return cont_consoante
+    else:
+        return cont_vogal
